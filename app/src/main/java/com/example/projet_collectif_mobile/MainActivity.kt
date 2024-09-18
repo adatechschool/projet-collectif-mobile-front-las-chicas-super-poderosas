6232ed5ce7a43c21f29e708b13f5e36bdde299e2
Adriana <adriana44@free.fr>
2024-09-18 16:42:00 +0200

    lien front back

--- app/src/main/java/com/example/projet_collectif_mobile/MainActivity.kt
+++ app/src/main/java/com/example/projet_collectif_mobile/MainActivity.kt
@@ -11,7 +11,14 @@ import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.example.projet_collectif_mobile.models.SurfSpot
+import com.example.projet_collectif_mobile.ui.screens.SpotsViewModel
+import com.example.projet_collectif_mobile.R
+import com.example.projet_collectif_mobile.SpotActivity
+
 
 
 
@@ -23,13 +30,16 @@ import com.google.gson.Gson
 
 
 class MainActivity : AppCompatActivity() {
+    // Utiliser la méthode viewModels pour instancier le ViewModel
+    private val spotsViewModel: SpotsViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Récupère les données du fichier JSON
-        val jsonString = ReadJSONFromAssets(baseContext, path = "spotsData.json")
+        //val jsonString = ReadJSONFromAssets(baseContext, path = "spotsData.json")
         // Convertit le JSON en objet Kotlin
-        val data = Gson().fromJson(jsonString, ClassJson::class.java)
+        //val data = Gson().fromJson(jsonString, ClassJson::class.java)
 
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -39,49 +49,59 @@ class MainActivity : AppCompatActivity() {
 
 
         //initialise Adapter = class native Android
-        val arrayAdapter: ArrayAdapter<ClassJson.SurfSpot> = object : ArrayAdapter<ClassJson.SurfSpot>(
-            this, R.layout.list_item, data.records
-        ) {
-            // surcharge méthode getView
-            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-                // Réutiliser les vues converties ou en créer de nouvelles si nécessaire
-                val inflater = LayoutInflater.from(context)
-                val view = convertView ?: inflater.inflate(R.layout.list_item, parent, false)
+        // val arrayAdapter: ArrayAdapter<ClassJson.SurfSpot> = object : ArrayAdapter<ClassJson.SurfSpot>(
+        //    this, R.layout.list_item, data.records
+        //  ) {
+
+        // Observer les changements de spotsUiState dans le ViewModel
+        spotsViewModel.spotsUiState.observe(this, Observer { spotsList ->
+            // spotsList contient la liste des spots, à utiliser dans l'ArrayAdapter
+
+            //initialise Adapter = class native Android
+            val arrayAdapter: ArrayAdapter<SurfSpot> = object : ArrayAdapter<SurfSpot>(
+                this, R.layout.list_item, spotsList
+            ) {
+                // surcharge méthode getView
+                override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                    // Réutiliser les vues converties ou en créer de nouvelles si nécessaire
+                    val inflater = LayoutInflater.from(context)
+                    val view = convertView ?: inflater.inflate(R.layout.list_item, parent, false)
 
 
-                // Récupérer l'objet Spot pour la position actuelle
-                val spot = getItem(position)
+                    // Récupérer l'objet Spot pour la position actuelle
+                    val spot = getItem(position)
 
-                // Récupérer les TextViews pour afficher le nom et l'emplacement
-                val spotNameTextView = view.findViewById<TextView>(R.id.spot_name)
-                val spotLocationTextView = view.findViewById<TextView>(R.id.spot_location)
-                val spotPictureView = view.findViewById<ImageView>(R.id.spot_img)
+                    // Récupérer les TextViews pour afficher le nom et l'emplacement
+                    val spotNameTextView = view.findViewById<TextView>(R.id.spot_name)
+                    val spotLocationTextView = view.findViewById<TextView>(R.id.spot_location)
+                    val spotPictureView = view.findViewById<ImageView>(R.id.spot_img)
 
-                // Remplir les TextViews avec les données de l'objet Spot
-                spotNameTextView.text = spot?.fields?.surfBreak?.joinToString(", ")
-                spotLocationTextView.text = spot?.fields?.Address
+                    // Remplir les TextViews avec les données de l'objet Spot
+                    spotNameTextView.text = spot?.surfBreak?.joinToString(", ")
+                    spotLocationTextView.text = spot?.Address
 
-                // Utilisation de Picasso pour charger l'image depuis l'URL - importer picasso
-                //Picasso.get().load(spot?.picture).into(spotPictureView)
+                    // Utilisation de Picasso pour charger l'image depuis l'URL - importer picasso
+                    //Picasso.get().load(spot?.picture).into(spotPictureView)
 
-                val button = view.findViewById<Button>(R.id.spot_btn)
-                button.setOnClickListener {
-                    // Créer un Intent pour lancer SpotDetailActivity
-                    val intent = Intent(applicationContext, SpotActivity::class.java)
+                    val button = view.findViewById<Button>(R.id.spot_btn)
+                    button.setOnClickListener {
+                        // Créer un Intent pour lancer SpotDetailActivity
+                        val intent = Intent(applicationContext, SpotActivity::class.java)
 
-                    // Ajouter les détails du Spot à l'Intent
-                    intent.putExtra("SPOT_NAME", spot?.fields?.surfBreak?.joinToString(", "))
-                    intent.putExtra("SPOT_LOCATION", spot?.fields?.Address)
+                        // Ajouter les détails du Spot à l'Intent
+                        intent.putExtra("SPOT_NAME", spot?.surfBreak?.joinToString(", "))
+                        intent.putExtra("SPOT_LOCATION", spot?.Address)
 
-                    // Lancer l'activité
-                    startActivity(intent)
+                        // Lancer l'activité
+                        startActivity(intent)
+                    }
+
+                    return view
                 }
-
-                return view
             }
-        }
 
-        listView.adapter = arrayAdapter
+            listView.adapter = arrayAdapter
 
-    }
-}
+        })
+        spotsViewModel.getSpotsList()
+    }}
